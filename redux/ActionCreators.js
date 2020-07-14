f74beaafc0388110e05a1df77977077f788875ab
Ha Rim Ku <hrku19@gmail.com>
2020-07-14 16:49:40 -0700

    Week 2 Workshop: comment Modal form and updating comment state in Redux store

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -152,3 +152,26 @@ export const addFavorite = campsiteId => ({
     type: ActionTypes.ADD_FAVORITE,
     payload: campsiteId
 });
+
+
+export const postComment = (campsiteId, rating, author, text) => dispatch => {
+    d = new Date();
+    date = d.toISOString();
+
+    const newComment = {
+        campsiteId,
+        rating,
+        author,
+        text,
+        date
+    };
+
+    setTimeout(() => {
+        dispatch(addComment(newComment));
+    }, 2000);
+};
+
+export const addComment = comment => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: comment
+});
